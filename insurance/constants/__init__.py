d17504dcde2fdc2c44080595ebbbb04c12606088
anil <anilsai029@gmail.com>
2022-08-16 13:12:43 +0530

    updated

--- insurance/constants/__init__.py
+++ insurance/constants/__init__.py
@@ -22,3 +22,23 @@ DATA_INGESTION_ZIP_DATASET_DIR_KEY = "zip_dataset_dir"
 DATA_INGESTION_INGESTED_DIR_NAME_KEY = "ingested_dir"
 DATA_INGESTION_TRAIN_DIR_KEY = "ingested_train_dir"
 DATA_INGESTION_TEST_DIR_KEY = "ingested_test_dir"
+
+# Data Validation related variables
+DATA_VALIDATION_CONFIG_KEY = "data_validation_config"
+DATA_VALIDATION_SCHEMA_FILE_NAME_KEY = "schema_file_name"
+DATA_VALIDATION_SCHEMA_DIR_KEY = "schema_dir"
+DATA_VALIDATION_ARTIFACT_DIR_NAME="data_validation"
+DATA_VALIDATION_REPORT_FILE_NAME_KEY = "report_file_name"
+DATA_VALIDATION_REPORT_PAGE_FILE_NAME_KEY = "report_page_file_name"
+
+
+## Data Transfromation related variables
+
+DATA_TRANSFORMATION_ARTIFACT_NAME = "data_tranformation"
+DATA_TRANSFORMATION_CONFIG_KEY   = "data_transformation_config"
+DATA_TRANSFORMATION_ADD_BEDROOM_PER_ROOM_KEY= "add_bedroom_per_room"
+DATA_TRANSFORMATION_TRANSFORMED_DIR_KEY= "transformed_dir"
+DATA_TRANSFORMATION_TRANSFORMED_TRAIN_DIR_KEY= "transformed_train_dir"
+DATA_TRANSFORMATION_TRANSFORMED_TEST_DIR_KEY= "transformed_test_dir"
+DATA_TRANSFORMATION_PREPROCESSING_DIR_KEY= "preprocessing_dir"
+DATA_TRANSFORMATION_PREPROCESSED_OBJECT_FILE_NAME_KEY= "preprocessed_object_file_name"
