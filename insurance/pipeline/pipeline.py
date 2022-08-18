6318bac1d148df2da29ce26a6f638a4a93078ae4
anil <anilsai029@gmail.com>
2022-08-18 17:21:47 +0530

    threading added to pipeline

--- insurance/pipeline/pipeline.py
+++ insurance/pipeline/pipeline.py
@@ -1,8 +1,13 @@
 
 
 
-import imp
+from collections import namedtuple
+from datetime import datetime
+import uuid
+import pandas as pd
+import os
 import sys
+from threading import Thread
 from insurance.entity.artifact_entity import ModelPusherArtifact,DataIngestionArtifact,DataValidationArtifact,ModelEvaluationArtifact, DataTransformationArtifact, ModelTrainerArtifact
 from insurance.entity.config_entity import DataIngestionConfig
 from insurance.logger import logging
@@ -14,11 +19,21 @@ from insurance.component.stage3_data_transformation import Data_Tranformation
 from insurance.component.stage4_model_trainer import ModelTrainer
 from insurance.component.stage5_model_evaluation import ModelEvaluation
 from insurance.component.stage6_model_pusher import ModelPusher
+from insurance.constants import EXPERIMENT_DIR_NAME, EXPERIMENT_FILE_NAME
 
-class Pipeline:
+Experiment = namedtuple("Experiment", ["experiment_id", "initialization_timestamp", "artifact_time_stamp",
+                                       "running_status", "start_time", "stop_time", "execution_time", "message",
+                                       "experiment_file_path", "accuracy", "is_model_accepted"])
+
+class Pipeline(Thread):
+    experiment: Experiment = Experiment(*([None] * 11))
+    experiment_file_path = None
 
     def __init__(self,config : Configuration = Configuration())-> None:
         try:
+            os.makedirs(config.training_pipeline_config.artifact_dir, exist_ok=True)
+            Pipeline.experiment_file_path=os.path.join(config.training_pipeline_config.artifact_dir,EXPERIMENT_DIR_NAME, EXPERIMENT_FILE_NAME)
+            super().__init__(daemon=False, name="pipeline")
             self.config = config
         except Exception as e:
             raise InsuranceException(e,sys)
@@ -92,6 +107,29 @@ class Pipeline:
 
     def run_pipeline(self):
         try:
+            if Pipeline.experiment.running_status:
+                logging.info("Pipeline is already running")
+                return Pipeline.experiment
+            # data ingestion
+            logging.info("Pipeline starting.")
+
+            experiment_id = str(uuid.uuid4())
+
+            Pipeline.experiment = Experiment(experiment_id=experiment_id,
+                                             initialization_timestamp=self.config.time_stamp,
+                                             artifact_time_stamp=self.config.time_stamp,
+                                             running_status=True,
+                                             start_time=datetime.now(),
+                                             stop_time=None,
+                                             execution_time=None,
+                                             experiment_file_path=Pipeline.experiment_file_path,
+                                             is_model_accepted=None,
+                                             message="Pipeline has been started.",
+                                             accuracy=None,
+                                             )
+            logging.info(f"Pipeline experiment: {Pipeline.experiment}")
+
+            self.save_experiment()
             data_ingestion_artifact = self.start_data_ingestion()
             data_validaion_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(
@@ -110,6 +148,64 @@ class Pipeline:
             else:
                 logging.info("Trained model rejected.")
             logging.info("Pipeline completed.")
+            
+            stop_time = datetime.now()
+            Pipeline.experiment = Experiment(experiment_id=Pipeline.experiment.experiment_id,
+                                             initialization_timestamp=self.config.time_stamp,
+                                             artifact_time_stamp=self.config.time_stamp,
+                                             running_status=False,
+                                             start_time=Pipeline.experiment.start_time,
+                                             stop_time=stop_time,
+                                             execution_time=stop_time - Pipeline.experiment.start_time,
+                                             message="Pipeline has been completed.",
+                                             experiment_file_path=Pipeline.experiment_file_path,
+                                             is_model_accepted=model_evaluation_artifact.is_model_accepted,
+                                             accuracy=model_trainer_artifact.model_accuracy
+                                             )
+            logging.info(f"Pipeline experiment: {Pipeline.experiment}")
+            self.save_experiment()
+        except Exception as e:
+            raise InsuranceException(e, sys) from e
 
+    def run(self):
+        try:
+            self.run_pipeline()
+        except Exception as e:
+            raise InsuranceException(e,sys) from e
+
+
+
+    def save_experiment(self):
+        try:
+            if Pipeline.experiment.experiment_id is not None:
+                experiment = Pipeline.experiment
+                experiment_dict = experiment._asdict()
+                experiment_dict: dict = {key: [value] for key, value in experiment_dict.items()}
+
+                experiment_dict.update({
+                    "created_time_stamp": [datetime.now()],
+                    "experiment_file_path": [os.path.basename(Pipeline.experiment.experiment_file_path)]})
+
+                experiment_report = pd.DataFrame(experiment_dict)
+
+                os.makedirs(os.path.dirname(Pipeline.experiment_file_path), exist_ok=True)
+                if os.path.exists(Pipeline.experiment_file_path):
+                    experiment_report.to_csv(Pipeline.experiment_file_path, index=False, header=False, mode="a")
+                else:
+                    experiment_report.to_csv(Pipeline.experiment_file_path, mode="w", index=False, header=True)
+            else:
+                print("First start experiment")
+        except Exception as e:
+            raise InsuranceException(e, sys) from e
+
+    @classmethod
+    def get_experiments_status(cls, limit: int = 5) -> pd.DataFrame:
+        try:
+            if os.path.exists(Pipeline.experiment_file_path):
+                df = pd.read_csv(Pipeline.experiment_file_path)
+                limit = -1 * int(limit)
+                return df[limit:].drop(columns=["experiment_file_path", "initialization_timestamp"], axis=1)
+            else:
+                return pd.DataFrame()
         except Exception as e:
             raise InsuranceException(e, sys) from e
