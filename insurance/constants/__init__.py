530ae0d55373801c7d7444924b7ad6711eaa6399
anil <anilsai029@gmail.com>
2022-08-18 09:54:19 +0530

    model pusher related configuration added

--- insurance/constants/__init__.py
+++ insurance/constants/__init__.py
@@ -75,7 +75,7 @@ MODEL_EVALUATION_FILE_NAME_KEY = "model_evaluation_file_name"
 MODEL_EVALUATION_ARTIFACT_DIR = "model_evaluation"
 
 
-# Model Pusher config key
+# Model Pusher config related variables
 MODEL_PUSHER_CONFIG_KEY = "model_pusher_config"
 MODEL_PUSHER_MODEL_EXPORT_DIR_KEY = "model_export_dir"
 
