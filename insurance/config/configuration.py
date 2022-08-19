c15bcdf2987af91e6277c78301ed5d6d554a674a
anil <anilsai029@gmail.com>
2022-08-19 22:41:23 +0530

    updated

--- insurance/config/configuration.py
+++ insurance/config/configuration.py
@@ -173,7 +173,7 @@ class Configuration:
 
     def get_model_pusher_cofig(self)-> ModelPusherConfig:
         try:
-            time_stamp = self.time_stamp
+            time_stamp = f"{datetime.now().strftime('%Y%m%d%H%M%S')}"
             model_pusher_config_info = self.config_info[MODEL_PUSHER_CONFIG_KEY]
             export_dir_path = os.path.join(ROOT_DIR, model_pusher_config_info[MODEL_PUSHER_MODEL_EXPORT_DIR_KEY],
                                            time_stamp)
