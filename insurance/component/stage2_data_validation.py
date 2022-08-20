3c3e021f0aceb00d54416c577c209f484c99ac8f
anil <anilsai029@gmail.com>
2022-08-20 11:39:25 +0530

    updated

--- insurance/component/stage2_data_validation.py
+++ insurance/component/stage2_data_validation.py
@@ -8,6 +8,7 @@ from evidently.model_profile import Profile
 from evidently.model_profile.sections import DataDriftProfileSection
 from evidently.dashboard import Dashboard
 from evidently.dashboard.tabs import DataDriftTab
+from insurance.utils.utils import read_yaml_file
 import json
 import pandas as pd
 
@@ -49,11 +50,45 @@ class DataValidation:
     def validate_dataset_schema(self) -> bool:
         try:
             validation_status = False
-            """
-            do the validation steps of the data set here
-            """
-            validation_status=True
-            return validation_status
+            logging.info(f"Checking if training and test file fits the schema.")
+
+            train_df = pd.read_csv(self.data_ingestion_artifact.train_file_path)
+            test_df = pd.read_csv(self.data_ingestion_artifact.test_file_path)
+
+            # reading column names from schema.yaml file
+            scheme_file_path = self.data_validation_config.schema_file_path
+            dict = read_yaml_file(filepath = scheme_file_path)['columns']
+            
+            schema_file_columns = []
+            for key in dict.keys():
+                schema_file_columns.append(key)
+            
+            logging.info(f"Reading column names from schema.yaml file: {schema_file_columns}")
+
+            # comparing column names of train, test and schema.yaml file
+            if sorted(train_df.columns.to_list()) == sorted(test_df.columns.to_list()) == sorted(schema_file_columns):
+
+                logging.info(f"Training, Testing and schema.yaml file having same column name.")
+                
+                # checking values of "sex", "region" in schema.yaml file
+                sex_yaml_col_value = sorted(read_yaml_file(filepath = scheme_file_path)['domain_value']['sex'])
+                region_yaml_col_value = sorted(read_yaml_file(filepath = scheme_file_path)['domain_value']['region'])
+
+                #checking values of "sex", "region" in train and test file
+                sex_val_train_df = sorted(train_df["sex"].unique())
+                sex_val_test_df = sorted(test_df["sex"].unique())
+
+                region_val_train_df = sorted(train_df["region"].unique())
+                region_val_test_df = sorted(test_df["region"].unique())
+
+                # checking whethere "sex", "region" column having same values or not
+                if sex_val_train_df == sex_val_test_df == sex_yaml_col_value:
+                    if region_val_train_df == region_val_test_df == region_yaml_col_value:
+                        validation_status = True
+                        logging.info(f'sex and region column hvaing same values in Training, Testing and schema.yaml file.')
+                logging.info(f" validation status of dataset_schema with schema.yaml file : {validation_status}")
+                return validation_status
+
         except Exception as e:
             raise InsuranceException(e,sys) from e
 
@@ -99,11 +134,13 @@ class DataValidation:
         except Exception as e:
             raise InsuranceException(e,sys) from e
 
-    def is_data_drift_found(self)->bool:
+    def is_data_drift_found(self):
         try:
             report = self.get_and_save_data_drift_report()
             self.save_data_drift_report_page()
-            return True
+            is_data_drift_found= report["data_drift"]["data"]["metrics"]["dataset_drift"]
+            logging.info(f"is data_drift found in the dataset : {is_data_drift_found}")
+            return is_data_drift_found
         except Exception as e:
             raise InsuranceException(e,sys) from e
 
